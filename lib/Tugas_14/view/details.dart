b1d3314802be5b9274117b013beb7a50035afa87
raulinsightlab <raulakbar1905@gmail.com>
2025-08-28 07:23:57 +0700

    tugas 14 selesai

--- lib/Tugas_14/view/details.dart
+++ lib/Tugas_14/view/details.dart
@@ -1,16 +1,151 @@
 import 'package:flutter/material.dart';
+import 'package:tugas1flutter/Tugas_14/model/user_model.dart';
 
-class DetailPage extends StatefulWidget {
-  const DetailPage({super.key});
+class DetailPage extends StatelessWidget {
+  final Welcome welcome;
+  const DetailPage({super.key, required this.welcome});
   static const id = "/page";
 
   @override
-  State<DetailPage> createState() => _DetailPageState();
-}
-
-class _DetailPageState extends State<DetailPage> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("Nama"));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Character Detail",
+          style: TextStyle(fontFamily: "Orbitron"),
+        ),
+        centerTitle: true,
+        // actions: [
+        //   Image.asset("name")
+        // ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(
+              // child: PhysicalModel(
+              //   color: Colors.transparent,
+              //   child: ClipRRect(borderRadius: BorderRadiusGeometry.circular(10),
+              //   child: Image.network(welcome.image ?? ""),
+              //   ),
+              // ),
+              child: Image.network(
+                welcome.imageUrl ?? "",
+                width: 300,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              padding: EdgeInsets.all(20),
+              width: double.infinity,
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Stack(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: Text(
+                          welcome.title ?? "",
+                          style: TextStyle(
+                            fontFamily: "Orbitron",
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Text(
+                            "Full Name :",
+                            style: TextStyle(fontFamily: "Orbitron"),
+                          ),
+                          SizedBox(width: 2),
+                          Text(
+                            welcome.fullName ?? "",
+                            style: TextStyle(fontFamily: "Orbitron"),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Text(
+                            "First Name :",
+                            style: TextStyle(fontFamily: "Orbitron"),
+                          ),
+                          SizedBox(width: 2),
+                          Text(
+                            welcome.firstName ?? "",
+                            style: TextStyle(fontFamily: "Orbitron"),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Text(
+                            "Last Name :",
+                            style: TextStyle(fontFamily: "Orbitron"),
+                          ),
+                          SizedBox(width: 2),
+                          Text(
+                            welcome.lastName ?? "",
+                            style: TextStyle(fontFamily: "Orbitron"),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Text(
+                            "Family :",
+                            style: TextStyle(fontFamily: "Orbitron"),
+                          ),
+                          SizedBox(width: 2),
+                          Text(
+                            welcome.family ?? "",
+                            style: TextStyle(fontFamily: "Orbitron"),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.all(20),
+              width: double.infinity,
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Text(
+                '"When You Play Game Of Thrones,\n You Win Or You Die"',
+                style: TextStyle(fontFamily: "Orbitron"),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Back", style: TextStyle(color: Colors.black)),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/Tugas_14/view/get_api.dart
+++ lib/Tugas_14/view/get_api.dart
@@ -10,18 +10,61 @@ class Tugas14API extends StatefulWidget {
 
   @override
   State<Tugas14API> createState() => _Tugas14APIState();
+  // List<Welcome> allusers;
+  // List<Welcome> filteredUsers;
 }
 
 class _Tugas14APIState extends State<Tugas14API> {
+  String query = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Game Of Thrones"), centerTitle: true),
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        // title: Text(
+        //   "Game Of Thrones",
+        //   style: TextStyle(fontFamily: "Orbitron"),
+        // ),
+        title: Image.asset(
+          "assets/images/logo_got-removebg-preview.png",
+          width: 300,
+          fit: BoxFit.cover,
+        ),
+        centerTitle: true,
+        actionsPadding: EdgeInsets.all(8),
+        // actions: [
+        //   // Image.asset(
+        //   //   "assets/images/icon_westeros.webp",
+        //   //   width: 25,
+        //   //   fit: BoxFit.cover,
+        //   // ),
+        //   CircleAvatar(backgroundImage: AssetImage("assets/images/dragon.jpg")),
+        // ],
+      ),
       body: Center(
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Padding(
+                padding: EdgeInsets.all(12.0),
+                child: TextField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    hintText: "Cari karakter berdasarkan nama...",
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      query = value.toLowerCase();
+                    });
+                  },
+                ),
+              ),
               FutureBuilder<List<Welcome>>(
                 future: getUser(),
                 builder: (BuildContext context, AsyncSnapshot snapshot) {
@@ -40,17 +83,49 @@ class _Tugas14APIState extends State<Tugas14API> {
                         itemCount: users.length,
                         itemBuilder: (BuildContext context, int index) {
                           final dataUser = users[index];
-                          return Card(
-                            elevation: 6,
+                          return
+                          // onTap: () {
+                          //   context.push(page)
+                          // },
+                          Card(
+                            color: Colors.black,
+                            elevation: 10,
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10),
                             ),
                             child: ListTile(
-                              leading: Image.network(dataUser.imageUrl ?? ""),
-                              title: Text(dataUser.fullName ?? ""),
+                              onTap: () {
+                                context.push(DetailPage(welcome: dataUser));
+                              },
+                              leading: Container(
+                                height: 60,
+                                width: 60,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  color: Colors.grey[900],
+                                ),
+                                clipBehavior: Clip.hardEdge,
+
+                                child: Image.network(
+                                  dataUser.imageUrl ?? "",
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              // CircleAvatar(
+                              //   radius: 30,
+                              //   backgroundImage: NetworkImage(
+                              //     dataUser.imageUrl ?? "",
+                              //   ),
+                              // ),
+                              // Image.network(dataUser.imageUrl ?? ""),
+                              title: Text(
+                                dataUser.fullName ?? "",
+                                style: TextStyle(color: Colors.white),
+                              ),
                               subtitle: Text(
-                                "${dataUser.firstName} ${dataUser.lastName}" ??
+                                "${dataUser.firstName} from ${dataUser.family}" ??
                                     "",
+                                style: TextStyle(color: Colors.grey),
                               ),
                             ),
                           );
